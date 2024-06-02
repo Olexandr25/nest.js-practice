50280ace6209a7d2baafbe4e51186b39737a8bc5
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2024-06-02 19:39:28 +0300

    feat: Add logger middleware to AppModule
    The code changes include:
    - Importing the `LoggerMiddleware` in the `AppModule`
    - Implementing the `NestModule` interface in the `AppModule`
    - Configuring the `LoggerMiddleware` to be applied to the `SongsController` routes
    
    This commit message follows the established convention of starting with a verb in the imperative form and providing a concise summary of the changes made.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsController } from './songs/songs.controller';
 import { SongsModule } from './songs/sonts.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController, SongsController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes('songs'); // 1
+
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes({ path: 'songs', method: RequestMethod.POST }); // 2
+
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); // 3
+  }
+}
